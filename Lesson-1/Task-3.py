ed250c6e5332011846911967e40421220ca2707c
Evgen_Palimov <palbimov@mail.ru>
2021-01-20 19:27:10 +0500

    Branch is clear

--- Lesson-1/Task-3.py
+++
@@ -1,9 +0,0 @@
-#Задача-3.
-#Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
-#Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
-
-user_number = input("Введите любое число: ")
-
-result = int(user_number) + int(user_number * 2) + int(user_number * 3)
-
-print(f"Сумма чисел {user_number} + {user_number * 2} + {user_number * 3} равняеться = {result}")
