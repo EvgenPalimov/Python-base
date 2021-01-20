4ebd0dc166346303e5d917516ce61a5f1a8823ce
Evgen_Palimov <palbimov@mail.ru>
2021-01-20 18:57:06 +0500

    Fixed a few bugs v2 in Lesson-1

--- Lesson-1/Task-3.py
+++ Lesson-1/Task-3.py
@@ -2,6 +2,8 @@
 #Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
 #Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
 
-n = int(input("Введите любое число: "))
-answer=(n + int(str(n) + str(n)) + int(str(n) + str(n)+ str(n)))
-print(f"Сумма чисел {n} + {str(n) + str(n)} + {str(n) + str(n) + str(n)} равняеться = {answer}")
+user_number = input("Введите любое число: ")
+
+result = int(user_number) + int(user_number * 2) + int(user_number * 3)
+
+print(f"Сумма чисел {user_number} + {user_number * 2} + {user_number * 3} равняеться = {result}")
