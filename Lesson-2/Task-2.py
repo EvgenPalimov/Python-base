ab7d58a5ca563a171bb6c4fc3346eb42b2d7070f
Evgen_Palimov <palbimov@mail.ru>
2021-01-26 23:37:32 +0500

    Change bags

--- Lesson-2/Task-2.py
+++ Lesson-2/Task-2.py
@@ -8,13 +8,12 @@
 my_list = []
 el_count = int(input("Введите количество элементов списка: "))
 i = 0
-el = 0
 
 while i < el_count:
     my_list.append(input("Введите следующее значение списка: "))
     i += 1
 
-
+el = 0
 for elem in range(int(len(my_list)/2)):
         my_list[el], my_list[el + 1] = my_list [el + 1], my_list[el]
         el += 2
