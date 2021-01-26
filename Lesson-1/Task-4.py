79a8193cd41e473784bc104d28e33e06384e441d
Evgen_Palimov <palbimov@mail.ru>
2021-01-26 22:03:48 +0500

    correct

---
+++ Lesson-1/Task-4.py
@@ -0,0 +1,16 @@
+#Задача-4.
+#Пользователь вводит целое положительное число. Найдите самую большую цифру в числе.
+#Для решения используйте цикл while и арифметические операции.
+
+user_number = int(input("Введите целое положтельное число: "))
+max = user_number % 10
+
+while user_number >= 1:
+    user_number = user_number // 10
+    if user_number % 10 > max:
+        max = user_number % 10
+    if user_number > 9:
+        continue
+    else:
+        print(f"Максимальное цифра в числе: {max}")
+        break
