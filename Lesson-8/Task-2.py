bfbb115d0c09cd499c7d725230aab87ed49bb929
Evgen_Palimov <palbimov@mail.ru>
2021-02-11 23:13:24 +0500

    Lesson-8\Task-2 is ready

---
+++ Lesson-8/Task-2.py
@@ -0,0 +1,42 @@
+"""
+Создайте собственный класс-исключение, обрабатывающий ситуацию деления на нуль.
+Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля в качестве делителя
+программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
+"""
+
+
+class MyError(Exception):
+    text = "Деление на ноль запрещено"
+
+    def __str__(self):
+        return self.text
+
+
+class NegativeNumber(Exception):
+    text = "Вы ввели отрицательное число!"
+
+    def __str__(self):
+        return self.text
+
+
+inp_data = int(input("Введите число: "))
+
+try:
+    if inp_data < 0:
+        raise NegativeNumber
+    if inp_data == 0:
+        raise MyError
+except ValueError:
+    print("Вы ввели не число!")
+except MyError as err:
+    print(err)
+except NegativeNumber as err_1:
+    print(err_1)
+else:
+    print(f"Все хорошо. Вы ввели число: {inp_data}.")
+
+
+
+
+
+
