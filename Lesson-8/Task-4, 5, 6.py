c45851b6dd7c1555eebd0bc9d52289a7f45085f2
Evgen_Palimov <palbimov@mail.ru>
2021-02-21 23:33:20 +0500

    Task-4, 5, 6/ Bags fixed

--- Lesson-8/Task-4, 5, 6.py
+++ Lesson-8/Task-4, 5, 6.py
@@ -23,45 +23,76 @@
 """
 
 
+class Store:
+    def __init__(self, initial_store=None):
+        self.store = initial_store if initial_store is not None else {}
+
+    def append(self, item, quantity):
+        assert isinstance(quantity, int)
+        assert quantity > 0
+        if item not in self.store:
+            self.store[item] = {"store": quantity}
+        else:
+            if "store" in self.store[item]:
+                self.store[item]["store"] += quantity
+            else:
+                self.store[item]["store"] = quantity
+
+    def move(self, item, departament, quantity):
+        assert isinstance(quantity, int)
+        assert quantity > 0
+        if item not in self.store:
+            raise ValueError("Нет такого оборудования на складе!")
+        if self.store[item].get('store', 0) < quantity:
+            raise ValueError("На складе нет нужного количества оборудования!")
+        self.store[item]["store"] -= quantity
+        if departament in self.store[item]:
+            self.store[item][departament] += quantity
+        else:
+            self.store[item][departament] = quantity
+
+
 class OfficeEquipment:
-    def __init__(self, name: str, price: int, quantity: int, modele: str = None, *args):
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-        self.modele = modele
-        self.my_unit = {'Модель устройства': self.name, 'Цена за ед': self.price, 'Количество': self.quantity}
+    def __init__(self, model, cost):
+        self.cost = cost
+        self.model = model
 
-    def reception(self):
-        unit = {'Модель устройства': self.name, 'Цена за ед': self.price, 'Количество': self.quantity,
-                'Тип техники': self.modele}
-        self.my_unit.update(unit)
-        return f'Ваше устройство добавлено на склад -\n {unit}', self.my_unit
-
-    def transfer(self):
-        pass
-    # Не знаю как реализоать данную функцию.
+    def __repr__(self):
+        return self.model
 
 
 class Printer(OfficeEquipment):
-    def __init__(self, *args):
-        super().__init__(modele='Принтер', *args)
+    def __init__(self, model, cost, max_print_size):
+        super().__init__(model, cost)
+        self.max_print_size = max_print_size
 
 
 class Scanner(OfficeEquipment):
-    def __init__(self, *args):
-        super().__init__(modele='Сканер', *args)
+    def __init__(self, model, cost, media_type):
+        super().__init__(model, cost)
+        self.media_type = media_type
 
 
-class Xerox(OfficeEquipment):
-    def __init__(self, *args):
-        super().__init__(modele='Ксерокс', *args)
+class Copier(OfficeEquipment):
+    def __init__(self, model, cost, color):
+        super().__init__(model, cost)
+        self.color = color
 
 
-unit_1 = Printer('HP', 2000, 5)
-unit_2 = Scanner('Canon', 1200, 5)
-unit_3 = Xerox('Xerox', 1500, 1)
-print(unit_1.reception())
-print(unit_2.reception())
-print(unit_3.reception())
+store = Store()
+item = Printer("hp1320", 5000, "A4")
 
+store.append(item, 10)
+print(store.store)
 
+store.move(item, "Продажи", 3)
+print(store.store)
+
+try:
+    store.move(item, "Производство", 9)
+    print(store.store)
+except Exception as e:
+    print(e)
+
+store.move(item, "Производство", 6)
+print(store.store)
